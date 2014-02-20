d72360c1d412b0641dd6e5fb5987f41a8854b2f7
Colm Ryan <cryan@bbn.com>
2014-02-20 14:11:05 -0500

    Initial commit of kernel driver.

---
+++ src/driver/PiecommDriver.c
@@ -0,0 +1,344 @@
+
+#include "PiecommDriver.h"
+
+
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_AUTHOR("Colm Ryan (Raytheon BBN)");
+MODULE_DESCRIPTION("Driver for PIECOMM Altera FPGA boards.");
+
+/* Forward Static PCI driver functions for kernel module */
+static int probe(struct pci_dev *dev, const struct pci_device_id *id);
+static void remove(struct pci_dev *dev);
+
+// static int  init_chrdev (struct aclpci_dev *aclpci);
+
+
+//Fill in kernel structures with a list of ids this driver can handle
+static struct pci_device_id piecomm_ids[] = {
+	{ PCI_DEVICE(VENDOR_ID, DEVICE_ID) },
+	{ 0, },
+};
+MODULE_DEVICE_TABLE(pci, piecomm_ids);
+
+
+static struct pci_driver piecommDriver = {
+	.name = DRIVER_NAME,
+	.id_table = piecomm_ids,
+	.probe = probe,
+	.remove = remove,
+};
+
+struct file_operations fileOps = {
+	.owner =    THIS_MODULE,
+	.read =     piecomm_read,
+	.write =    piecomm_write,
+	.open =     piecomm_open,
+	.release =  piecomm_close,
+};
+
+
+/*I/0 - should move to separate file at some point */
+struct IOCmd_t {
+	uint32_t cmd; //command word
+	uint8_t barNum; //which bar we are read/writing to
+	uint32_t devAddr; // address relative to BAR that we are read/writing to
+	void * userAddr; // virtual address in user space to read/write from
+};
+
+ssize_t rw_dispatcher(struct file *filePtr, char __user *buf, size_t count, bool rwFlag){
+
+	//Read the command from the buffer
+	struct IOCmd_t iocmd; 
+	void * startAddr;
+
+	uint8_t val8;
+	ssize_t retVal = 0;
+
+	struct DevInfo_t * devInfo = (struct DevInfo_t *) filePtr->private_data;
+
+	printk(KERN_INFO "PIECOMM: rw_dispatcher: Entering function.\n");
+
+	if (down_interruptible(&devInfo->sem)) {
+		printk(KERN_WARNING "PIECOMM: rw_dispatcher: Unable to get semaphore!\n");
+		return -1;
+	}
+
+	copy_from_user(&iocmd, (void __user *) buf, sizeof(iocmd));
+
+	//Map the device address to the iomaped memory
+	startAddr = (void*) (devInfo->bar[iocmd.barNum] + iocmd.devAddr) ;
+
+	if (rwFlag) {
+	
+		printk(KERN_INFO "PIECOMM: rw_dispatcher: Reading %u bytes from 0x%p.\n", (unsigned int) count, iocmd.userAddr);
+		switch (count){
+			case 1:
+				val8 = ioread8(startAddr);
+				printk(KERN_INFO "Read 0x%x from 0x%p", val8, startAddr);
+				copy_to_user(iocmd.userAddr, &val8, 1);
+				retVal = 1;
+				break;
+			default:
+				retVal = 0;
+
+		}
+	}
+	else{
+
+		printk(KERN_INFO "PIECOMM: rw_dispatcher: Writing %u bytes from 0x%p.\n", (unsigned int) count, iocmd.userAddr);
+
+		switch (count){
+			case 1:
+				copy_from_user(&val8, iocmd.userAddr, 1);
+				printk(KERN_INFO "Writing 0x%x to 0x%p\n", val8, startAddr);
+				iowrite8(val8, startAddr);
+				retVal = 1;
+				break;
+			default:
+				retVal = 0;
+		}
+
+
+
+	}
+	up(&devInfo->sem);
+	return 0;
+}
+
+int piecomm_open(struct inode *inode, struct file *filePtr) {
+	//Get a handle to our devInfo and store it in the file handle
+	struct DevInfo_t * devInfo = 0;
+
+	printk(KERN_INFO "PIECOMM: piecomm_open: Entering function.\n");
+
+	devInfo = container_of(inode->i_cdev, struct DevInfo_t, cdev);
+
+	if (down_interruptible(&devInfo->sem)) {
+		printk(KERN_WARNING "PIECOMM: piecomm_open: Unable to get semaphore!\n");
+		return -1;
+	}
+
+	filePtr->private_data = devInfo;
+
+	//Record the PID of who opened the file
+	//TODO: sort out where this is used
+	devInfo->userPID = current->pid;
+
+	//Return semaphore
+	up(&devInfo->sem);
+
+	if (down_interruptible(&devInfo->sem)) {
+		printk(KERN_WARNING "PIECOMM: piecomm_open: Unable to get semaphore!\n");
+		return -1;
+	}
+
+	up(&devInfo->sem);
+
+	printk(KERN_INFO "PIECOMM: piecomm_open: Leaving function.\n");
+
+	return 0;
+}
+int piecomm_close(struct inode *inode, struct file *filePtr){
+
+	struct DevInfo_t * devInfo = (struct DevInfo_t *)filePtr->private_data;
+
+	if (down_interruptible(&devInfo->sem)) {
+		printk(KERN_WARNING "PIECOMM: piecomm_close: Unable to get semaphore!\n");
+		return -1;
+	}
+
+	//TODO: some checking of who is closing.
+	up(&devInfo->sem);
+
+	return 0;
+}
+
+//Pass-through to main dispatcher
+ssize_t piecomm_read(struct file *filePtr, char __user *buf, size_t count, loff_t *pos){
+
+	return rw_dispatcher(filePtr, buf, count, true);
+}
+ssize_t piecomm_write(struct file *filePtr, const char __user *buf, size_t count, loff_t *pos){
+
+		return rw_dispatcher(filePtr, (char __user *) buf, count, false);
+}
+
+static int setup_chrdev(struct DevInfo_t *devInfo){
+	/*
+	Setup the /dev/deviceName to allow user programs to read/write to the driver.
+	*/
+
+	int devMinor = 0;
+	int devMajor = 0; 
+	int devNum = -1;
+
+	int result = alloc_chrdev_region(&devInfo->cdevNum, devMinor, 1 /* one device*/, BOARD_NAME);
+	if (result < 0) {
+		printk(KERN_WARNING "Can't get major ID\n");
+		return -1;
+	}
+	devMajor = MAJOR(devInfo->cdevNum);
+	devNum = MKDEV(devMajor, devMinor);
+	
+	//Initialize and fill out the char device structure
+	cdev_init(&devInfo->cdev, &fileOps);
+	devInfo->cdev.owner = THIS_MODULE;
+	devInfo->cdev.ops = &fileOps;
+	result = cdev_add(&devInfo->cdev, devNum, 1 /* one device */);
+	if (result) {
+		printk(KERN_NOTICE "Error %d adding char device for PIECOMM driver with major/minor %d / %d", result, devMajor, devMinor);
+		return -1;
+	}
+
+	return 0;
+}
+
+static int map_bars(struct DevInfo_t *devInfo){
+	/*
+	Map the device memory regions into kernel virtual address space.
+	Report their sizes in devInfo.barLengths
+	*/
+	int ct = 0;
+	unsigned long barStart, barEnd, barLength;
+	for (ct = 0; ct < NUM_BARS; ct++){
+		printk(KERN_INFO "PIECOMM: Trying to map BAR #%d of %d.\n", ct, NUM_BARS);
+		barStart = pci_resource_start(devInfo->pciDev, ct);
+		barEnd = pci_resource_end(devInfo->pciDev, ct);
+		barLength = barEnd - barStart + 1;
+
+		devInfo->barLengths[ct] = barLength;
+
+		//Check for empty BAR
+		if (!barStart || !barEnd) {
+			devInfo->barLengths[ct] = 0;
+			printk(KERN_INFO "PIECOMM: Empty BAR #%d.\n", ct);
+			continue;
+		}
+
+		//Check for messed up BAR
+		if (barLength < 1) {
+			printk(KERN_WARNING "BAR #%d length is less than 1 byte.\n", ct);
+			continue;
+		}
+
+		// If we have a valid bar region then map the device memory or
+		// IO region into kernel virtual address space  
+		devInfo->bar[ct] = pci_iomap(devInfo->pciDev, ct, barLength);
+
+		if (!devInfo->bar[ct]) {
+			printk(KERN_WARNING "Could not map BAR #%d.\n", ct);
+			return -1;
+		}
+
+		printk(KERN_INFO "BAR[%d] mapped at 0x%p with length %lu.\n", ct, devInfo->bar[ct], barLength);
+	}
+	return 0;
+}  
+
+static int unmap_bars(struct DevInfo_t * devInfo){
+	/* Release the mapped BAR memory */
+	int ct = 0;
+	for (ct = 0; ct < NUM_BARS; ct++) {
+		if (devInfo->bar[ct]) {
+			pci_iounmap(devInfo->pciDev, devInfo->bar[ct]);
+			devInfo->bar[ct] = NULL;
+		}
+	}
+	return 0;
+}
+
+static int probe(struct pci_dev *dev, const struct pci_device_id *id) {
+		/*
+		From : http://www.makelinux.net/ldd3/chp-12-sect-1
+		This function is called by the PCI core when it has a struct pci_dev that it thinks this driver wants to control.
+		A pointer to the struct pci_device_id that the PCI core used to make this decision is also passed to this function. 
+		If the PCI driver claims the struct pci_dev that is passed to it, it should initialize the device properly and return 0. 
+		If the driver does not want to claim the device, or an error occurs, it should return a negative error value.
+		*/
+
+		//Initalize driver info 
+		struct DevInfo_t *devInfo = 0;
+
+		printk(KERN_INFO "PIECOMM: Entered driver probe function.\n");
+		printk(KERN_INFO "PIECOMM: vendor = 0x%x, device = 0x%x \n", dev->vendor, dev->device); 
+
+		//Allocate and zero memory for devInfo
+
+		devInfo = kzalloc(sizeof(struct DevInfo_t), GFP_KERNEL);
+		if (!devInfo) {
+			printk(KERN_WARNING "Couldn't allocate memory for device info!\n");
+			return -1;
+		}
+
+		//Copy in the pci device info
+		devInfo->pciDev = dev;
+
+		//Save the device info itself into the pci driver
+		dev_set_drvdata(&dev->dev, (void*) devInfo);
+
+		//Setup the char device
+		setup_chrdev(devInfo);    
+
+		//Initialize other fields
+		devInfo->userPID = -1;
+		devInfo->buffer = kmalloc (BUFFER_SIZE * sizeof(char), GFP_KERNEL);
+
+		//Enable the PCI
+		if (pci_enable_device(dev)){
+			printk(KERN_WARNING "PIECOMM: pci_enable_device failed!\n");
+			return -1;
+		}
+
+		pci_set_master(dev);
+		pci_request_regions(dev, DRIVER_NAME);
+
+		//Memory map the BAR regions into virtual memory space
+		map_bars(devInfo);
+
+		//TODO: proper error catching and memory releasing
+		sema_init(&devInfo->sem, 1);
+
+		return 0;
+
+}
+
+static void remove(struct pci_dev *dev) {
+
+	struct DevInfo_t *devInfo = 0;
+	
+	printk(KERN_INFO "Entered PIECOMM driver remove function.\n");
+	
+	devInfo = (struct DevInfo_t*) dev_get_drvdata(&dev->dev);
+	if (devInfo == 0) {
+		printk(KERN_WARNING "PIECOMM remove: devInfo is 0");
+		return;
+	}
+
+	//Clean up the char device
+	cdev_del(&devInfo->cdev);
+	unregister_chrdev_region(devInfo->cdevNum, 1);
+
+	//Release memory
+	unmap_bars(devInfo);
+
+	//TODO: does order matter here?
+	pci_release_regions(dev);
+	pci_disable_device(dev);
+
+	kfree(devInfo->buffer);
+	kfree(devInfo);
+
+}
+
+static int piecomm_init(void){
+	printk(KERN_INFO "PIECOMM: Loading PIECOMM driver!\n");
+	return pci_register_driver(&piecommDriver);
+}
+
+static void piecomm_exit(void){
+	printk(KERN_INFO "PIECOMM: Exiting PIECOMM driver!\n");
+	pci_unregister_driver(&piecommDriver);
+}
+
+module_init(piecomm_init);
+module_exit(piecomm_exit);
---
+++ src/driver/PiecommDriver.h
@@ -0,0 +1,59 @@
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/pci.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/uaccess.h>
+#include <linux/sched.h>
+
+
+#define VENDOR_ID 0x1172
+#define DEVICE_ID 0x0004
+
+#define DRIVER_NAME "piecomm"
+#define BOARD_NAME "piecomm1"
+#define NUM_BARS 3  //we use up to BAR2
+
+
+//fileio.c functions for device
+int piecomm_open(struct inode *inode, struct file *file);
+int piecomm_close(struct inode *inode, struct file *file);
+ssize_t piecomm_read(struct file *file, char __user *buf, size_t count, loff_t *pos);
+ssize_t piecomm_write(struct file *file, const char __user *buf, size_t count, loff_t *pos);
+
+
+/* Maximum size of driver buffer (allocated with kalloc()).
+ * Needed to copy data from user to kernel space */
+static const size_t BUFFER_SIZE = PAGE_SIZE;
+
+//Keep track of bits and bobs that we need for the driver
+struct DevInfo_t {
+  /* the kernel pci device data structure */
+  struct pci_dev *pciDev;
+  
+  /* upstream root node */
+  struct pci_dev *upstream;
+  
+  /* kernel's virtual addr. for the mapped BARs */
+  void * __iomem bar[NUM_BARS];
+  
+  /* length of each memory region. Used for error checking. */
+  size_t barLengths[NUM_BARS];
+
+  /* temporary buffer. If allocated, will be BUF_SIZE. */
+  char *buffer;
+  
+  /* Mutex for this device. */
+  struct semaphore sem;
+  
+  /* PID of process that called open() */
+  int userPID;
+  
+  /* character device */
+  dev_t cdevNum;
+  struct cdev cdev;
+  struct class *myClass;
+  struct device *device;
+
+};
