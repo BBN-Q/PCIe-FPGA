d3d8404a9780f43b799f92c9f3b90c5fb58ce782
Colm Ryan <cryan@bbn.com>
2014-02-24 10:51:52 -0500

    Actually add the load_driver shell script.

---
+++ src/driver/load_driver.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#Bash script to load BBN FPGA PCIe driver.  Must be run with sudo rights. 
+
+# Load the BBN FPGA PCIe driver
+if [ `lsmod | grep -o BBN_FPGA` ]; then
+	echo "BBN FPGA driver has already been loaded. Doing nothing."
+	exit
+fi
+insmod BBN_FPGA.ko
+
+#Find what major device number was assigned from /proc/devices
+majorNum=$( awk '{ if ($2 ~ /piecomm1/) print $1}' /proc/devices )
+
+if [ -z "$majorNum" ]; then
+	echo "Unable to find the BBN_FPGA device!"
+	echo "Did the driver correctly load?"
+else
+	#Remove any stale device file
+	if [ -e "/dev/piecomm1" ]; then
+		rm -r /dev/piecomm1
+	fi
+
+	#Create a new one with full read/write permissions for everyone
+	sudo mknod -m 666 /dev/piecomm1 c $majorNum 0
+fi
+
+
