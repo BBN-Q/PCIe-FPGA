3bf511a4669637ff3df9a46447ead1b61f23fe87
Colm Ryan <cryan@bbn.com>
2014-04-04 15:04:23 -0400

    WIP: basic CSR with dual-port BRAM

--- src/fpga/BBN_Pulse_Counter_top.vhd
+++ src/fpga/BBN_Pulse_Counter_top.vhd
@@ -35,10 +35,21 @@ signal reconfig_togxb : std_logic_vector(3 downto 0) := (others => '0');
 signal reconfigBusy : std_logic := '0';
 
 
+--Streaming data
 signal data_in_data               : std_logic_vector(31 downto 0) := (others => '0'); 
 signal data_in_valid              : std_logic                     := '0';             
 signal data_in_ready              : std_logic                     := '0';                    
 
+--CSR signals
+signal csrReadData, csrWriteData : std_logic_vector(31 downto 0) := (others => '0');
+signal csrAddr : unsigned(5 downto 0) := (others => '0') ;
+signal csrWE : std_logic := '0';
+
+signal controlWord : std_logic_vector(31 downto 0) ;
+
+--Pulse counts
+type PulseCountArray_t is array(0 to 15) of unsigned(31 downto 0) ;
+signal pulseCounts : PulseCountArray_t := (others => (others => '0'));
 
 begin
 
@@ -97,8 +108,96 @@ qsys : entity streaming_sgdma.streaming_sgdma
         data_in_clk_reset_reset_n => resetn,
         data_in_data              => data_in_data,
         data_in_valid             => data_in_valid,
-        data_in_ready             => data_in_ready);
+        data_in_ready             => data_in_ready,
 
+        --On chip memory as poor-man's CSR
+        onchip_memory2_0_clk2_clk => clk_100MHz,
+        onchip_memory2_0_s2_address => x"00" & std_logic_vector(csrAddr),
+        onchip_memory2_0_s2_chipselect => '1',
+        onchip_memory2_0_s2_clken => pllLocked,
+        onchip_memory2_0_s2_write => csrWE,
+        onchip_memory2_0_s2_readdata => csrReadData,
+        onchip_memory2_0_s2_writedata => csrWriteData,
+        onchip_memory2_0_s2_byteenable => (others => '1'),
+        onchip_memory2_0_reset2_reset => not resetn,
+        onchip_memory2_0_reset2_reset_req => not resetn
+    );
+
+
+--Poor man's CSR, scan through first 32 words 
+-- csr : process( resetn, clk_100Mhz )
+
+-- begin
+-- 	if resetn = '0' then
+-- 		csrWriteData <= (others => '0');
+-- 		csrAddr <= (others => '0');
+-- 		csrWE <= '0';
+-- 		controlWord <= (others => '0');
+-- 	elsif rising_edge(clk_100MHz) then
+-- 		if (csrAddr >= to_unsigned(31, 14)) then
+-- 			csrAddr <= (others => '0');
+-- 		else
+-- 			csrAddr <= csrAddr + 1;
+-- 		end if;
+
+-- 		if (csrAddr < 16) then
+-- 			--Reading
+-- 			csrWE <= '0';
+-- 			case( csrAddr(4 downto 0) ) is
+			
+-- 				when b"00001" =>
+-- 					controlWord <= csrReadData;
+-- 				when others =>
+-- 					null;
+-- 			end case ;
+-- 		else
+-- 			--Writing
+-- 			csrWE <= '1';
+-- 			csrWriteData <= std_logic_vector(pulseCounts(to_integer(csrAddr - 16)));
+-- 		end if;		
+-- 	end if;
+-- end process ; -- csr
+
+
+csr : process( resetn, clk_100MHz )
+begin
+	if resetn = '0' then
+ 		csrWriteData <= (others => '0');
+ 		csrAddr <= (others => '0');
+ 		csrWE <= '0';
+ 		controlWord <= (others => '1');
+ 	elsif rising_edge(clk_100Mhz) then
+
+ 		csrAddr <= csrAddr + 1;
+
+ 		if (csrAddr < 16) then 
+	 		csrWE <= '0';
+	 		if (csrAddr = b"00110") then
+	 			controlWord <= csrReadData;
+	 		end if;
+	 	else
+			csrWE <= '1';
+			csrWriteData <= std_logic_vector(pulseCounts(to_integer(csrAddr - 16)));
+		end if;
+	 		
+	end if;
+
+end process ; -- csr
+
+
+-- 0 - 0 
+-- 1 - 4
+-- 2 - 8
+-- 3 - C  -- with variable come out here
+-- 4 - 10
+-- 5 - 14 -- with signal comes out here
+-- 6 - 1C
+
+-- csrWriteData <= (others => '0');
+-- csrAddr <= (others => '0');
+leds <= not controlWord(3 downto 0);
+
+--Count pulses coming in 
 
 
 
@@ -115,28 +214,35 @@ begin
 	end if;
 end process ; -- fakeData
 
--- data_in_data <= x"BADDF00F";
--- data_in_valid <= '1';
+pulseCounts(0) <= x"BAADF00F";
+pulseCounts(1) <= x"BAADF11F";
+pulseCounts(2) <= x"BAADF22F";
+pulseCounts(3) <= x"BAADF33F";
+pulseCounts(4) <= x"BAADF44F";
 
 --For now put a slow counter out on the LEDS so we know the board alive
 
-countpro : process( resetn, clk_100MHz )
+-- countpro : process( resetn, clk_100MHz )
 
-variable counter : unsigned(24 downto 0) := (others => '0');
-variable slowCounter : unsigned(3 downto 0) := (others => '0');
+-- variable counter : unsigned(31 downto 0) := (others => '0');
+-- variable slowCounter : unsigned(3 downto 0) := (others => '0');
 
-begin
-	leds <= not std_logic_vector(slowCounter);
-	if resetn = '0' then
-		counter := (others => '0');
-		slowCounter := (others => '0');
-	elsif rising_edge(clk_100MHz) then
-		counter := counter + 1;
-		if counter = 0 then
-			slowCounter := slowCounter + 1;
-		end if;
-	end if;
-end process ; -- counter
+-- begin
+-- 	leds <= not std_logic_vector(slowCounter);
+-- 	if resetn = '0' then
+-- 		counter := (others => '0');
+-- 		slowCounter := (others => '0');
+-- 	elsif rising_edge(clk_100MHz) then
+-- 		if (counter < unsigned(controlWord)) then
+-- 			counter := counter + 1;
+-- 		else
+-- 			counter := (others => '0');
+-- 		end if;
+-- 		if counter = 0 then
+-- 			slowCounter := slowCounter + 1;
+-- 		end if;
+-- 	end if;
+-- end process ; -- counter
 
 
 
